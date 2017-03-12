e82fe3e38b772d15ad57d25fc5701406be048039
Allie Vogel <alliemvogel@gmail.com>
2017-03-12 14:51:04 +0200

    new changes

--- chatbot/models.py
+++ chatbot/models.py
@@ -8,25 +8,30 @@ from django.db import models
 class Users(models.Model):
     user_name = models.CharField(max_length=200)
     set_cookie = models.IntegerField(default=0)
+    session_id = models.IntegerField(default=0)
 
     def __str__(self):
         return self.user_name
 
 
 class Brief(models.Model):
+    brief_type = models.CharField(max_length=200)
 
+    def __str__(self):
+        return self.brief_type
 
 
 class Questions(models.Model):
     question_ID = models.IntegerField(default=0)
     keyword_goal = models.CharField(max_length=200)
-    question_text =
+    question_text = models.CharField(max_length=200)
 
     def __str__(self):
         return self.keyword_goal
 
 class Answers(models.Model):
     question_ID = models.ForeignKey(Questions, on_delete=models.CASCADE)
+    answer_text = models.CharField(max_length=200)
 
     def __str__(self):
         return self.question_ID
